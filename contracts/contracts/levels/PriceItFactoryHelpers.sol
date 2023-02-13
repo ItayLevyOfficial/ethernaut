87781ac052f249f4f6ba41e027443e2979120bd1
itaylevy <itaylevy134@gmail.com>
2023-02-13 12:14:12 +0200

    keep working

--- contracts/contracts/levels/PriceItFactoryHelpers.sol
+++ contracts/contracts/levels/PriceItFactoryHelpers.sol
@@ -7,11 +7,7 @@ import "./PriceIt.sol";
 contract MockedUniswapV2Factory {
   mapping(address => mapping(address => address)) public getPair;
 
-  function createPair(address tokenA, address tokenB) external returns (address pair) {
-    require(tokenA != tokenB, "UniswapV2: IDENTICAL_ADDRESSES");
-    (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-    require(token0 != address(0), "UniswapV2: ZERO_ADDRESS");
-    require(getPair[token0][token1] == address(0), "UniswapV2: PAIR_EXISTS");
+  function createPair(address token0, address token1) external returns (address pair) {
     bytes memory bytecode = type(UniswapV2Pair).creationCode;
     bytes32 salt = keccak256(abi.encodePacked(token0, token1));
     assembly {
@@ -27,12 +23,7 @@ contract MockedUniswapV2Factory {
 contract MockedUniswapV2Router {
   address public immutable factory;
 
-  modifier ensure(uint256 deadline) {
-    require(deadline >= block.timestamp, "UniswapV2Router: EXPIRED");
-    _;
-  }
-
-  constructor(address _factory) public {
+  constructor(address _factory) {
     factory = _factory;
   }
 
@@ -45,7 +36,7 @@ contract MockedUniswapV2Router {
     uint256 amountBMin,
     address to,
     uint256 deadline
-  ) external virtual ensure(deadline) {
+  ) external virtual {
     address pair = IUniswapV2Factory(factory).getPair(tokenA, tokenB);
     IERC20(tokenA).transferFrom(msg.sender, pair, amountADesired);
     IERC20(tokenB).transferFrom(msg.sender, pair, amountADesired);
@@ -57,8 +48,6 @@ contract MockedUniswapV2Router {
     uint256 reserveIn,
     uint256 reserveOut
   ) public pure virtual returns (uint256 amountOut) {
-    require(amountIn > 0, "UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT");
-    require(reserveIn > 0 && reserveOut > 0, "UniswapV2Library: INSUFFICIENT_LIQUIDITY");
     uint256 amountInWithFee = amountIn * 997;
     uint256 numerator = amountInWithFee * reserveOut;
     uint256 denominator = (reserveIn * 1000) + (amountInWithFee);
@@ -67,7 +56,7 @@ contract MockedUniswapV2Router {
 }
 
 contract TestingERC20 is Ownable, ERC20 {
-  constructor(string memory _name, string memory _symbol) public Ownable() ERC20(_name, _symbol) {}
+  constructor(string memory _name, string memory _symbol) Ownable() ERC20(_name, _symbol) {}
 
   function mint(address account, uint256 amount) external onlyOwner {
     _mint(account, amount);
