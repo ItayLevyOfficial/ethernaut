76c9fbc778430613f1776ac3a62e192e84a342ce
itaylevy <itaylevy134@gmail.com>
2023-04-14 12:58:30 +0300

    Now the PriceItFactoryHelpers looks great

--- contracts/contracts/levels/PriceItFactoryHelpers.sol
+++ contracts/contracts/levels/PriceItFactoryHelpers.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.5.0;
 
 import { ERC20 } from "openzeppelin-contracts-05/token/ERC20/ERC20.sol";
-import { Ownable } from "openzeppelin-contracts-05/access/Ownable.sol";
+import { ERC20Detailed } from "openzeppelin-contracts-05/token/ERC20/ERC20Detailed.sol";
+import { Ownable } from "openzeppelin-contracts-05/ownership/Ownable.sol";
 
-contract TestingERC20 is Ownable, ERC20 {
-  constructor(string memory _name, string memory _symbol) Ownable() ERC20(_name, _symbol) {}
+contract TestingERC20 is Ownable, ERC20, ERC20Detailed {
+  constructor(string memory _name, string memory _symbol) public Ownable() ERC20Detailed(_name, _symbol, 18) {}
 
   function mint(address account, uint256 amount) external onlyOwner {
     _mint(account, amount);
